aca4e59902dcf44ccb6f852fbe2b086c1c70d67d
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-04-10 01:23:49 +0530

    Regular Expression Intro

--- Regular Expression/src/Main.java
+++ Regular Expression/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String string = "I am a String. Yes, I am.";
+        System.out.println(string);
+
+        String yourString = string.replaceAll("I", "You");
+        System.out.println(yourString);
+
+        String alphaNumeric = "ajacjksma123132";
+//        System.out.println(alphaNumeric.replaceAll(".", "Y"));
+
+        System.out.println(alphaNumeric.replaceAll("^ajacjksma", "I am"));
+
+        System.out.println(alphaNumeric.matches("^hello"));
+
+        System.out.println(alphaNumeric.replaceAll("123132$", " you!"));
+
+        System.out.println(alphaNumeric.replaceAll("[aei]", "X"));
+
+        System.out.println(alphaNumeric.replaceAll("[aei][Fj]", "X"));
     }
 }
