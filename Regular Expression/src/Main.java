0263f84f291c882032a9aef299ea92386f2c6d4a
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-04-10 08:44:20 +0530

    Character classes and Boundry Machers

--- Regular Expression/src/Main.java
+++ Regular Expression/src/Main.java
@@ -18,5 +18,35 @@ public class Main {
         System.out.println(alphaNumeric.replaceAll("[Hhi]", "X"));
 
         System.out.println(alphaNumeric.replaceAll("[H][r]", "X"));
+
+        System.out.println("Harry".replaceAll("[Hh]arry", "Harry"));
+
+        String newAlphaNumeric = "Hrishi36-25";
+
+        System.out.println(newAlphaNumeric.replaceAll("[^is]", "X"));
+
+        System.out.println(newAlphaNumeric.replaceAll("[H-i3-5]", "X"));
+
+        System.out.println(newAlphaNumeric.replaceAll("[1-5]", "X"));
+
+        System.out.println(newAlphaNumeric.replaceAll("\\d", "X"));
+
+        System.out.println(newAlphaNumeric.replaceAll("\\D", "X"));
+
+        String hasWhitespace = "I have blanks and\ta tab, and also a newLine\n";
+
+        System.out.println(hasWhitespace);
+
+        System.out.println(hasWhitespace.replaceAll("\\s", ""));
+
+        System.out.println(hasWhitespace.replaceAll("\t", ""));
+
+        System.out.println(hasWhitespace.replaceAll("\\S", ""));
+
+        System.out.println(newAlphaNumeric.replaceAll("\\w", "X"));
+
+        System.out.println(hasWhitespace.replaceAll("\\w", "X"));
+
+        System.out.println(hasWhitespace.replaceAll("\\b", "X"));
     }
 }
