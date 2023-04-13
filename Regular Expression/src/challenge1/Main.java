2423b07b7ee0a46d5d64d0a66c888f70a8e7f23c
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-04-13 23:49:09 +0530

    challenge 3 and challenge 4

--- Regular Expression/src/challenge1/Main.java
+++ Regular Expression/src/challenge1/Main.java
@@ -1,18 +1,38 @@
 package challenge1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
         String challenge1 = "I want a bike.";
         System.out.println(challenge1.matches("I want a bike."));
 
+        System.out.println("\nRegEx 1");
         String regEXp = "I want a \\w+.";
         System.out.println(challenge1.matches(regEXp));
         String challenge2 = "I want a ball.";
         System.out.println(challenge2.matches(regEXp));
 
+        System.out.println("\nRegEx 2");
         String regExp1 = " I want a (bike|ball).";
         System.out.println(challenge1.matches(regExp1));
         System.out.println(challenge2.matches(regExp1));
+
+        System.out.println("\nRegEx 3");
+        String regExp3 = "I want a \\w+.";
+        Pattern pattern = Pattern.compile(regExp3);
+        Matcher matcher = pattern.matcher(challenge1);
+        System.out.println(matcher.matches());
+
+        matcher = pattern.matcher(challenge2);
+        System.out.println(matcher.matches());
+
+//  Replace all occurrences of blank with an underscore for the following String. Print out the resulting string.
+        String challenge4 = "Replace all banks with underscores";
+        System.out.println(challenge4.replaceAll(" ", "_"));
+        System.out.println(challenge4.replaceAll("\\s", "_"));
+
 
     }
 }
