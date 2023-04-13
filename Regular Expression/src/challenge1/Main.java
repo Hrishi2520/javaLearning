a5a389be66e669b9d77abb61721de998e6a550ce
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-04-14 00:05:03 +0530

    challenge 7

--- Regular Expression/src/challenge1/Main.java
+++ Regular Expression/src/challenge1/Main.java
@@ -40,5 +40,9 @@ public class Main {
 
         System.out.println(challenge5.matches("^a{3}bc{8}d{3}ef{3}g$"));
         System.out.println(challenge5.replaceAll("^a{3}bc{8}d{3}ef{3}g$", "REPLACED"));
+
+        //challenge7
+        String challenge7  = "abcd.135";
+        System.out.println(challenge7.matches("^[A-z][a-z]+\\.\\d+$"));
     }
 }
