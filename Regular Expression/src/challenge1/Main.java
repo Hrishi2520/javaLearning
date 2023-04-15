3c9346f71601de391fad8ad8b85de0cf37374c27
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-04-15 23:57:21 +0530

    updated code structure

--- Regular Expression/src/challenge1/Main.java
+++ Regular Expression/src/challenge1/Main.java
@@ -42,45 +42,45 @@ public class Main {
         System.out.println(challenge5.replaceAll("^a{3}bc{8}d{3}ef{3}g$", "REPLACED"));
 
         //challenge7
-        String challenge7  = "abcd.135";
+        String challenge7 = "abcd.135";
         System.out.println(challenge7.matches("^[A-z][a-z]+\\.\\d+$"));
 
         //challenge 8
         String challenge8 = "abcd.135uvqz.7tzik.999";
         Pattern pattern8 = Pattern.compile("[A-Za-z]+\\.(\\d+)");
         Matcher matcher8 = pattern8.matcher(challenge8);
-        while(matcher8.find()) {
-            System.out.println("Occurrence: "+matcher8.group(1));
+        while (matcher8.find()) {
+            System.out.println("Occurrence: " + matcher8.group(1));
         }
 
         //challenge 9
         String challenge9 = "abcd.135\tuvqz.7\ttzik.999\n";
         Pattern pattern9 = Pattern.compile("[A-Za-z]+\\.(\\d+)\\s");
         Matcher matcher9 = pattern9.matcher(challenge9);
-        while(matcher9.find()) {
-            System.out.println("Occurrence: "+matcher9.group(1));
+        while (matcher9.find()) {
+            System.out.println("Occurrence: " + matcher9.group(1));
         }
         //challenge 10
         String challenge10 = "abcd.135\tuvqz.7\ttzik.999\n";
         Pattern pattern10 = Pattern.compile("[A-Za-z]+\\.(\\d+)\\s");
         Matcher matcher10 = pattern10.matcher(challenge10);
-        while(matcher10.find()) {
-            System.out.println("Occurrence: start = "+matcher10.start(1) + " end = " + (matcher10.end(1) - 1));
+        while (matcher10.find()) {
+            System.out.println("Occurrence: start = " + matcher10.start(1) + " end = " + (matcher10.end(1) - 1));
         }
 
         //challenge 11
         String challenge11 = "{0, 2}, {0, 5}, {1,3}, {2, 4}";
         Pattern pattern11 = Pattern.compile("\\{(.+?)\\}");
         Matcher matcher11 = pattern11.matcher(challenge11);
-        while(matcher11.find()) {
-            System.out.println("Occurrence: "+ matcher11.group(1));
+        while (matcher11.find()) {
+            System.out.println("Occurrence: " + matcher11.group(1));
         }
         //challenge 12
         String challenge12 = "{0, 2}, {0, 5}, {1,3}, {2, 4}, {x, y}, {2, 4}, {11, 12}";
         Pattern pattern12 = Pattern.compile("\\{(\\d+, \\d+)\\}");
         Matcher matcher12 = pattern12.matcher(challenge12);
-        while(matcher12.find()) {
-            System.out.println("Occurrence: "+ matcher12.group(1));
+        while (matcher12.find()) {
+            System.out.println("Occurrence: " + matcher12.group(1));
         }
 
         //challenge13
