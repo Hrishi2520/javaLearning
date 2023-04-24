1ce5c990a04c28b3adbe2fe6f1717a5b581a3eba
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-04-24 11:52:12 +0530

    junit challange 8 - 10

--- UnitTesting/src/challange/UtilitiesTest.java
+++ UnitTesting/src/challange/UtilitiesTest.java
@@ -6,9 +6,15 @@ import static org.junit.Assert.*;
 
 public class UtilitiesTest {
 
+    Utilities util;
+
+    @org.junit.Before
+    public void setup() {
+        util = new Utilities();
+    }
+
     @Test
     public void everyNthChar() {
-        Utilities util = new Utilities();
         char[] output = util.everyNthChar(new char[] {'h','e','l','l','o'}, 2);
         assertArrayEquals(new char[] {'e','l'}, output);
         char[] output2 = util.everyNthChar(new char[] {'h','e','l','l','o'}, 8);
@@ -17,7 +23,6 @@ public class UtilitiesTest {
 
     @Test
     public void removePairs() throws Exception {
-        Utilities util = new Utilities();
         assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
         assertNull("did not get null returned when argument passed was null", util.removePairs(null));
         assertEquals("A", util.removePairs("A"));
@@ -25,14 +30,17 @@ public class UtilitiesTest {
     }
 
     @Test
-    public void converter() {
+    public void converter() throws Exception{
         Utilities util = new Utilities();
         assertEquals(300, util.converter(10, 5));
     }
 
+    @org.junit.Test(expected = ArithmeticException.class)
+    public void converter_arithmeticException() throws Exception {
+        util.converter(10, 0);
+    }
     @Test
     public void nullIfOddLength() {
-        Utilities util = new Utilities();
         assertNull(util.nullIfOddLength("odd"));
         assertNotNull(util.nullIfOddLength("even"));
     }
