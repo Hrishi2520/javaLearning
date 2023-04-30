7021fe965d726a47b49dd75909fb1ba38558730a
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-04-30 15:41:15 +0530

    java query for artists

---
+++ TestDB/src/music/model/Main.java
@@ -0,0 +1,27 @@
+package music.model;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DataSource dataSource = new DataSource();
+
+        if (!dataSource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        List<Artist> artists = dataSource.queryArtists();
+        if (artists == null) {
+            System.out.println("No Artists!");
+            return;
+        }
+
+        for (Artist artist : artists) {
+            System.out.println("ID = "+artist.getId()+", Name = "+ artist.getName());
+        }
+
+        dataSource.close();
+    }
+}
